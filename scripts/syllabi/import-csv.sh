2aaec25eec4ef357bc44fbddcd7818657c1ed02a
George K. Thiruvathukal <gkt@cs.luc.edu>
2020-10-10 12:21:11 -0500

    show the SQL query for getting the info needed for CSV table

--- scripts/syllabi/import-csv.sh
+++ scripts/syllabi/import-csv.sh
@@ -4,5 +4,6 @@ sqlite3 courses.db << EOF
 .mode csv
 .import courses.csv courses
 .schema
-select * from courses;
+select "COMP Course Number" || '-' || "COMP Section Number", "Faculty Last Name", Semester, Syllabus from courses where Semester="Spring 2020" and "Final Version" = 'Yes' order by Semester, "COMP Course Number";
 EOF
+
