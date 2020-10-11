a1822031d1e43ead9c2b935df339249c33618c11
George K. Thiruvathukal <gkt@cs.luc.edu>
2020-10-10 23:11:32 -0500

    fetch data from public read-only Google Sheet

--- scripts/syllabi/import-csv.sh
+++ scripts/syllabi/import-csv.sh
@@ -1,9 +1,21 @@
 #!/bin/bash
 
+# Note:
+# This link is coming from Google Sheets
+# File -> Publish to Web 
+
+echo "Getting Google Sheets Syllabi"
+
+export LINK="https://docs.google.com/spreadsheets/d/e/2PACX-1vT1R7FirTrxj-_gD0LUj_gKPxF9myqvwvxdX109beNrw0wS9P-KI4mGDMEZg1x35w8UrIMNwkx4yDDR/pub?gid=715548665&single=true&output=csv"
+wget -O courses.csv "$LINK"
+
+echo "Importing into Local DB"
+
+rm -f courses.db
 sqlite3 courses.db << EOF
 .mode csv
 .import courses.csv courses
 .schema
-select "COMP Course Number" || '-' || "COMP Section Number", "Faculty Last Name", Semester, Syllabus from courses where Semester="Spring 2020" and "Final Version" = 'Yes' order by Semester, "COMP Course Number";
+select "COMP Course Number" || '-' || "COMP Section Number", "Faculty Last Name", Semester, Syllabus from courses where "Final Version" = 'Yes' order by Semester, "COMP Course Number";
 EOF
 
