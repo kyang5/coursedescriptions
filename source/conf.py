0dabbace4c7707dced88d8b25a4972f6e749fe43
Nicholas M. Synovic <nicholas.synovic@gmail.com>
2021-07-20 12:59:28 +0000

    Ran isort

--- source/conf.py
+++ source/conf.py
@@ -11,8 +11,8 @@
 # All configuration values have a default; values that are commented out
 # serve to show the default.
 
-import sys, os
-
+import os
+import sys
 from datetime import date
 
 # version = os.environ.get("BOOK_VERSION", "beta")
